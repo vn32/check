896725167a612aaf02a4d37697d2d6f988dc72f7
vn32 <nishadvikas87@gmial.com>
2020-07-27 09:36:34 +0530

    Added react particles in background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import React,{Component} from 'react';
+
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
+import Particles from 'react-particles-js';
+const particlesOptions={
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}  
 class App extends Component{
   render(){
     return(
       <div className="App">
+      <Particles params={particlesOptions}/>
       <Navigation/>
       <Logo/>
       <Rank/>
