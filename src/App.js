c35aea329023c9360133f3c16f1808d64c384570
vn32 <nishadvikas87@gmial.com>
2020-07-27 08:44:11 +0530

    Added ImageLinkForm component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React,{Component} from 'react';
 import './App.css';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 class App extends Component{
   render(){
     return(
       <div className="App">
       <Navigation/>
       <Logo/>
+      <ImageLinkForm/>
       
       </div>
     );
---
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ImageLinkForm=()=>{
+    return(
+        <div>
+            <p className='fa3'>
+                {'This magic brain will detect faces in your pictures.Give it a try'}
+            </p> 
+            <div className='center'>
+                <div className='center pa4 br3 shadow-5'>
+                    <input className='fa4 pa2 w-70 center' type="text"/>
+                    <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'>
+                        Detect
+                    </button>
+                </div>
+            </div>
+       </div>
+       
+    );
+}
+export default ImageLinkForm;
